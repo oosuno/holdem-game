1aff698579a09356028c273e5a13158e7ae06e43
oosuno <oosuno@gmail.com>
2026-05-22 19:33:34 +0900

    Add files via upload

--- server.js
+++ server.js
@@ -150,4 +150,4 @@ io.on('connection', (socket) => {
 });
 
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log(`서버 실행 중` house));
+server.listen(PORT, () => console.log(`서버 실행 중: 포트 ${PORT}`));
